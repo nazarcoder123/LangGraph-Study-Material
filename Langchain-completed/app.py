14d83df39d281e951bce62ea4be6cc6019950823
unknown <nazarchess14@gmail.com>
2025-10-06 23:05:58 +0530

    w

--- Langchain-completed/app.py
+++
@@ -1,5 +0,0 @@
-# Create a LANGSMITH_API_KEY in Settings > API Keys
-from langsmith import Client
-client = Client(api_key="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")
-prompt = client.pull_prompt("hwchase17/openai-functions-agent", include_model=True)
-print(prompt)
